021eb5df56041e22e324989a798a2e2a42337469
Kristian A Hafell <kristianhafell@gmail.com>
2024-04-26 13:27:16 +0200

    add table testing

--- apps/learn-test/src/main.spec.ts
+++ apps/learn-test/src/main.spec.ts
@@ -1,4 +1,4 @@
-import { add, graiterThan} from './main';
+import { add, graiterThan, getItemsFromTable, setItemsInTable, itemIsInTable, deleteItemInTableById, streamConnectionOnline } from './main';
 
 describe('add', () => {
     it('should add two numbers', () => {
@@ -15,3 +15,52 @@ describe('graiterThan', () => {
         expect(graiterThan(1, 2)).toEqual(false);
     });
 })
+
+describe('logTable-test', () => {
+    const tableName = 'logTable-6e5f217';
+    const connection = { endpoint: 'https://jyb0pgmaq3.execute-api.eu-north-1.amazonaws.com/dev', ConnectionId: 'W1JeIfxkAi0CF7Q='};
+    it('should return 404 if item not found', async () => {
+        const items = await getItemsFromTable('table-not-exist');
+        expect(items.statusCode).toEqual(404);
+        console.log("items: ", items.body);
+    });
+    it('should return items from table', async () => {
+        const items = await getItemsFromTable(tableName);
+        expect(items.statusCode).toEqual(200);
+        console.log("items: ", items.body);
+    });
+
+    it('should set items in table', async () => {
+        const items = await setItemsInTable(tableName, {id:"test", message: "test", metric: "test"});
+        expect(items.statusCode).toEqual(200);
+        console.log("items: ", items.body);
+    });
+    
+    it('should return 404 if item not table', async () => {
+        const items = await itemIsInTable(tableName, 'gsfhjjkadlwukbefsdcv');
+        expect(items.statusCode).toEqual(404);
+        console.log("items: ", items.body);
+    });
+    it('should return 200 if item in table', async () => {
+        const items = await itemIsInTable(tableName, 'test');
+        expect(items.statusCode).toEqual(200);
+        console.log("items: ", items.body);
+    });
+
+    it('should delete item in table by id', async () => {
+        const items = await deleteItemInTableById(tableName, 'test');
+        expect(items.statusCode).toEqual(200);
+        console.log("items: ", items.body);
+    });
+
+    it('should return 500 if conecting to stream', async () => {
+        const items = await streamConnectionOnline({ endpoint: 'https://wrong/dev', ConnectionId: 'wrong'});
+        expect(items.statusCode).toEqual(500);
+    });
+    it('should return 200 if conecting to stream', async () => {
+        const items = await streamConnectionOnline(connection);
+        console.log("items: ", items.body);
+        expect(items.statusCode).toEqual(200);
+    });
+
+});
