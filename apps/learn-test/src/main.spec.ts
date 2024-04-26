fbf80b3dc776a2be5108b98d0559a59b19bfafac
Kristian A Hafell <kristianhafell@gmail.com>
2024-04-26 13:26:36 +0200

    add simpe testing

--- apps/learn-test/src/main.spec.ts
+++ apps/learn-test/src/main.spec.ts
@@ -1,7 +1,17 @@
-import { add } from './main';
+import { add, graiterThan} from './main';
 
 describe('add', () => {
     it('should add two numbers', () => {
         expect(add(1, 2)).toEqual(3);
     });
 });
+
+describe('graiterThan', () => {
+    it('should return true if a is greater than b', () => {
+        expect(graiterThan(2, 1)).toEqual(true);
+    });
+
+    it('should return false if a is less than b', () => {
+        expect(graiterThan(1, 2)).toEqual(false);
+    });
+})
--- apps/learn-test/src/main.ts
+++ apps/learn-test/src/main.ts
@@ -1,4 +1,11 @@
+import { getItems, setItems, deleteItem, } from "@logger/logger-sdk";
+
+
 
 export function add(a: number, b: number): number {
     return a + b;
+}
+
+export function graiterThan(a: number, b: number): boolean {
+    return a > b;
 }
